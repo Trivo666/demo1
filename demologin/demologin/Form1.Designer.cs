a32631a4fe509c0a2aa36fcc91e3d50fce9516e7
Trivo666 <86337398+Trivo666@users.noreply.github.com>
2022-03-09 01:41:32 +0700

    commit

---
+++ demologin/demologin/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿
+namespace demologin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbluser = new System.Windows.Forms.Label();
+            this.txtuser = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lbluser
+            // 
+            this.lbluser.AutoSize = true;
+            this.lbluser.Location = new System.Drawing.Point(166, 68);
+            this.lbluser.Name = "lbluser";
+            this.lbluser.Size = new System.Drawing.Size(53, 13);
+            this.lbluser.TabIndex = 0;
+            this.lbluser.Text = "username";
+            // 
+            // txtuser
+            // 
+            this.txtuser.Location = new System.Drawing.Point(243, 65);
+            this.txtuser.Name = "txtuser";
+            this.txtuser.Size = new System.Drawing.Size(228, 20);
+            this.txtuser.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtuser);
+            this.Controls.Add(this.lbluser);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbluser;
+        private System.Windows.Forms.TextBox txtuser;
+    }
+}
+
